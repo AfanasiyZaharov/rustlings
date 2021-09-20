57a20ec434bfc2ff9fecb177f1ae12503bb51530
Afanasiy Zakharov <af.zakharov@rambler-co.ru>
2021-09-20 16:35:40 +0300

    some work have done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -35,6 +34,35 @@ impl Default for Person {
 // Otherwise, then return an instantiated Person object with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let values: Vec<&str> = s.split(",").collect();
+        if(values.len() < 2){
+            return Person::default();
+        }
+        let mut name: String;
+        let mut age: usize;
+        if values[0].len() == 0 {
+            return Person::default();
+        } else {
+            name = values[0].to_string();
+        }
+        if values[1].len() == 0 {
+            return Person::default();
+        } else {
+            let res = values[1].parse::<usize>();
+            if let Ok(val) = res{
+                age = val;
+            } else {
+                return Person::default();
+            }
+        }
+
+        Person{
+            age,
+            name,
+        }
     }
 }
 
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,32 @@
 // iterators4.rs
 
-// I AM NOT DONE
+
+struct FactorialCounter{
+    curVal: u64,
+    steps: u64,
+}
+
+impl FactorialCounter {
+    fn new() -> FactorialCounter {
+        FactorialCounter { curVal: 1, steps: 1 }
+    }
+}
+
+impl Iterator for FactorialCounter {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.steps > 100 {
+            return None;
+        }
+        self.steps += 1;
+        self.curVal = self.curVal * self.steps;
+        Some(self.curVal)
+    }
+}
 
 pub fn factorial(num: u64) -> u64 {
+    (1..=num).fold(1, |acc, v| acc * v)
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
